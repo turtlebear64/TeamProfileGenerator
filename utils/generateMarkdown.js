c3ddec2b0bc88693a41c5e395dd9e0cfc6b32786
Nicolas Danciu <nixrom1@gmail.com>
2022-02-16 19:54:35 -0500

    uploading files for the team profile generator

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,11 @@
+function renderLicenseBadge(license) {}
+function renderLicenseLink(license) {}
+
+function renderLicenseSection(license) {}
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
